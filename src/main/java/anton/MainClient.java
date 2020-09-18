669ec993a85aaa90fd4eb9062f9bc91ff92dea30
Anton Ovchinnikov <t2da@mail.ru>
2020-09-18 21:09:43 +0300

    first commit

---
+++ src/main/java/anton/MainClient.java
@@ -0,0 +1,20 @@
+package anton;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MainClient {
+    public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int port = 8080;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Hello Server!!))");
+        }
+    }
+}
+
